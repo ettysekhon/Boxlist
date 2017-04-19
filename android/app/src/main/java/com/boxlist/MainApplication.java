c06d19dbb1e7f3d91adc1ee1844bc4984f1e9042
Etty Sekhon <sekhoe01@118-UX.local>
2017-04-20 00:21:04 +0100

    add key

--- android/app/src/main/java/com/boxlist/MainApplication.java
+++ android/app/src/main/java/com/boxlist/MainApplication.java
@@ -4,9 +4,6 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.idehub.GoogleAnalyticsBridge.GoogleAnalyticsBridgePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -29,10 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNDeviceInfo(),
-            new VectorIconsPackage(),
-            new MapsPackage(),
-            new GoogleAnalyticsBridgePackage()
+          new GoogleAnalyticsBridgePackage()
       );
     }
   };
